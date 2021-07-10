5932fc2f5ae62a9575395b4875b4f18a10763e62
ghostforpy <ghostformobile@gmail.com>
2021-07-10 13:51:08 +0300

    .

--- super_project/users/models.py
+++ super_project/users/models.py
@@ -1,5 +1,6 @@
+from django.contrib.auth import models
 from django.contrib.auth.models import AbstractUser
-from django.db.models import CharField
+from django.db.models import CharField, ImageField
 from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
 
@@ -11,6 +12,7 @@ class User(AbstractUser):
     name = CharField(_("Name of User"), blank=True, max_length=255)
     first_name = None  # type: ignore
     last_name = None  # type: ignore
+    image = ImageField(upload_to='users/%Y/%m/%d', blank=True)
 
     def get_absolute_url(self):
         """Get url for user's detail view.
